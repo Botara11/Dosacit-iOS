55a9f7895db6b214cbd5c77509043b5277a75f77
serne01 <serne01@gmail.com>
2015-03-18 13:37:22 +0100

    Base de datos comprobando boquillas

--- DosApp/Cultivo1.swift
+++ DosApp/Cultivo1.swift
@@ -1,7 +1,4 @@
-//  HOLA BORJA
-//  Created by Borja Tarazona Raga on 20/1/15.
-//  Copyright (c) 2015 Borja Tarazona Raga. All rights reserved.
-//
+
 
 import Foundation
 import UIKit
@@ -13,6 +10,8 @@ import CoreData
 let managedObjectContext = (UIApplication.sharedApplication().delegate as AppDelegate).managedObjectContext
 
 let newItem = NSEntityDescription.insertNewObjectForEntityForName("A1", inManagedObjectContext: managedObjectContext!) as A1
+
+
 
 
 class Cultivo1: UIViewController {
@@ -72,7 +71,7 @@ class Cultivo1: UIViewController {
         else {
             return nil
         }
-        }()
+        }() 
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!) {
         
@@ -193,15 +192,103 @@ class Cultivo1: UIViewController {
             let count = boquillas.count
             
             for item in boquillas{
+                
                 boq = item.componentsSeparatedByString("%%%")
-                var k = Double()
-                k = (boq[0] as NSString).doubleValue
+                if boq.count == 5 {
+                    let dbBoquillas = NSEntityDescription.insertNewObjectForEntityForName("EntityBoquillas", inManagedObjectContext: self.managedObjectContext!) as EntityBoquillas
+                    
+                let k = Double((boq[4] as NSString).doubleValue / pow(10,0.5))
+                let dia = (boq[2] as NSString).doubleValue
+
+                dbBoquillas.marca = boq[0]
+                dbBoquillas.modelo = boq[1]
+                dbBoquillas.diametro = dia
+                dbBoquillas.caudal = (boq[4] as NSString).doubleValue
+                dbBoquillas.p6 = k * pow(6,0.5)
+                dbBoquillas.p7 = k * pow(7,0.5)
+                dbBoquillas.p8 = k * pow(8,0.5)
+                dbBoquillas.p9 = k * pow(9,0.5)
+                dbBoquillas.p10 = k * pow(10,0.5)
+                dbBoquillas.p11 = k * pow(11,0.5)
+                dbBoquillas.p12 = k * pow(12,0.5)
+                dbBoquillas.p13 = k * pow(13,0.5)
+                dbBoquillas.p14 = k * pow(14,0.5)
+                dbBoquillas.p15 = k * pow(15,0.5)
+                dbBoquillas.p16 = k * pow(16,0.5)
+                
+                
+
+                }
+
+            }
+            println(boquillas[0])
+/*
+            let fetchRequest = NSFetchRequest(entityName: "EntityBoquillas")
+            let fetchResults1 = self.managedObjectContext!.executeFetchRequest(fetchRequest, error: nil) as? [EntityBoquillas]
+            *//*
+            for var ue=0; ue<fetchResults1?.count; ue++ {
+                println(fetchResults1?[ue].marca)
+                println(fetchResults1?[ue].modelo)
+                println(fetchResults1?[ue].caudal)
+                println(fetchResults1?[ue].p6)
+                println(fetchResults1?[ue].p7)
+                println(fetchResults1?[ue].p16)
                 
             }
-            println(boquillas)
+            */
             
             
+            /*********  AQUI EMPIEZA  **********/
             
+            // Mirar: http://www.codingexplorer.com/getting-started-uitableview-swift/
+            
+            var marcasList: [String] = ["Teejet","Hardi","Albuz","Lechler","Discos","Otros"]
+            
+            var presionList: [String] = ["p6","p7","p8","p9","p10","p11","p12","p13","p14","p15","p16"]
+            
+           let fetchRequest = NSFetchRequest(entityName: "EntityBoquillas")
+            
+            let sortDescriptor = NSSortDescriptor(key: "marca", ascending: true)
+            
+            fetchRequest.sortDescriptors = [sortDescriptor]
+            
+            let intervaloCaudalAdmisible = [0.3,1.2,0.3,1.2,0.3,1.2] as [Double]
+            
+            for marca in marcasList {
+                for presion in presionList {
+                    //let predicate1 = NSPredicate(format: "(%@ >= %f) AND (%@<= %f) AND (Marca == %@)",   presion, intervaloCaudalAdmisible[0], presion, intervaloCaudalAdmisible[1], i)
+                    
+                
+                    let str = (presion + " > %f AND " + presion + "< %f AND marca == %@"  ) as String
+                    let predicate1 = NSPredicate(format: str , intervaloCaudalAdmisible[0], intervaloCaudalAdmisible[1], marca)
+                    fetchRequest.predicate = predicate1
+                    if let fetchResults1 = self.managedObjectContext!.executeFetchRequest(fetchRequest, error: nil) as? [EntityBoquillas]{
+                        println(fetchResults1[0].marca)
+                        println(fetchResults1[0].modelo)
+                        println(fetchResults1[0].p6)
+                        let predicate2 = NSPredicate(format: "(%@ >= %f) AND (%@<= %f) AND (Marca == %@)","p6", intervaloCaudalAdmisible[2], "p6", intervaloCaudalAdmisible[3], marca)
+                        fetchRequest.predicate = predicate2
+                        let fetchResults2 = self.managedObjectContext!.executeFetchRequest(fetchRequest, error: nil) as? [EntityBoquillas]
+                        if (fetchResults2 != nil){
+                            let predicate3 = NSPredicate(format: "(%@ >= %f) AND (%@<= %f) AND (Marca == %@)",presion, intervaloCaudalAdmisible[4], presion, intervaloCaudalAdmisible[5], marca)
+                            fetchRequest.predicate = predicate3
+                            let fetchResults3 = self.managedObjectContext!.executeFetchRequest(fetchRequest, error: nil) as? [EntityBoquillas]
+                            if (fetchResults3 != nil){
+                                /** Si hay resultados en las 3 zonas... **/
+                                
+                                println(marca)
+                            }
+                        }
+                    }
+                }
+            }
+            
+            
+           /* if  {
+                logItems = fetchResults
+            }
+            */
+            /*********  AQUI TERMINA  **********/
             
         }
         
@@ -219,12 +306,14 @@ class Cultivo1: UIViewController {
         
         //HAY DATOS QUE ESTAN VACIOS "" ASI QUE NO SE PUEDE HACER DOUBLE.PARSEDOUBLE()
         Double k = Double.parseDouble(boq[4])/java.lang.Math.pow(10.0,0.5);
+        
         //MARCA - MODELO - DIAMETRO(puede no haber) - CAUDAL
         double dia;
         if(boq[2].compareTo("")!=0){
-        dia = Double.parseDouble(boq[2]);
+            dia = Double.parseDouble(boq[2]);
         }else
-        dia = 0.0;
+            dia = 0.0;
+        
         //System.out.println(boq[1] + " "+boq[4]+"/("+boq[4]+"^0,5) = " +k);
         db.addBoquilla(boq[0],boq[1],dia,Double.parseDouble(boq[4]),k*java.lang.Math.pow(6.0,0.5),
         k*java.lang.Math.pow(7.0,0.5),
